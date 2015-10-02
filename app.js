a34ea8e4d28d1c891b82f1cbe3b3a0deae8e597f
warren <warrendevs@gmail.com>
2015-10-02 15:58:54 -0700

    Adds hogan-express and path modules. Sets up more express app configuration (app.js) including initial routing, view engine configuration, and paths.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.route('/').get(function(req, res, next){
-    res.send('<h1>Hello World!</h1>');
+//    res.send('<h1>Hello World!</h1>')
+    res.render('index', {title: 'Welcome to TemplateApp'});
 });
 
 app.listen(3000, function(){
-    console.log('templateApp running on Port 3000');
+    console.log('TemplateApp running on Port 3000');
 });
